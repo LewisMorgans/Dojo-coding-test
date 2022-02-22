2ef877f78217e3104d143d310545c7ebc31f3c31
Lewis Morgans <lewis.morgans@srt-marine.com>
2022-02-22 11:47:50 +0000

    repurpose template to support new requirements. Move subscriber into class

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { CountriesApiService } from '../../services/countries-api.service';
-import { Observable } from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import { Country } from '../../models/country.model';
 import { Countries } from '../../models/countries.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.scss'],
 })
-export class CountriesComponent {
-  public countries$: Observable<Countries> = this.countriesApiService.getCountries$();
-  public selectedCountry: Country = null;
+export class CountriesComponent implements AfterViewInit {
+  public countries: Countries;
+  public selectedCountry: Object;
+  public countryCount = 0;
 
   constructor(private readonly countriesApiService: CountriesApiService) {}
+
+  ngAfterViewInit() {
+   this.mapper();
+  }
+
+  async mapper(): Promise<Countries> {
+    let countryIndex = 0;
+    return this.countries = await this.countriesApiService.getCountries$()
+      .pipe(
+        map((country) => {
+          for (let countryKey in country) {
+            this.countryCount = countryIndex++
+          }
+          return country;
+        })
+      )
+      .toPromise();
+
+  }
 }
